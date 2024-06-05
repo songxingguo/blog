664a0d3e5624d2ed08484d1b2351a39c68ce8539
songxingguo <xg.song@qq.com>
2024-06-05 14:15:17 +0800

    feat: 更改 pubDate

--- src/pages/atom.xml.ts
+++ src/pages/atom.xml.ts
@@ -1,18 +1,17 @@
-import rss from '@astrojs/rss';
-import { getPosts } from '~/utils';
+import rss from "@astrojs/rss";
+import { getPosts } from "~/utils";
 import { THEME_CONFIG } from "~/theme.config";
-import type { APIContext } from 'astro';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
+import type { APIContext } from "astro";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
 
 const parser = new MarkdownIt();
 
-const { title, desc, website, author } = THEME_CONFIG
-
+const { title, desc, website, author } = THEME_CONFIG;
 
 export async function GET(_context: APIContext) {
-  const posts = await getPosts()
-  const allowedTags = sanitizeHtml.defaults.allowedTags.concat(['img'])
+  const posts = await getPosts();
+  const allowedTags = sanitizeHtml.defaults.allowedTags.concat(["img"]);
   return rss({
     title: title,
     description: desc,
@@ -21,17 +20,17 @@ export async function GET(_context: APIContext) {
       return {
         link: `/posts/${post.slug}/`,
         author: author,
-        content: sanitizeHtml(parser.render(post.body), { allowedTags, }),
+        content: sanitizeHtml(parser.render(post.body), { allowedTags }),
         title: post.data.title,
-        pubDate: post.data.pubDate,
+        pubDate: post.data.date,
         description: post.data.description,
         customData: post.data.customData,
         categories: post.data.categories,
         commentsUrl: post.data.commentsUrl,
         source: post.data.source,
         enclosure: post.data.enclosure,
-      }
+      };
     }),
-    stylesheet: '/pretty-feed-v3.xsl',
+    stylesheet: "/pretty-feed-v3.xsl",
   });
 }
