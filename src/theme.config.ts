3716a8a58dd37433192dbdb93901df83c174c283
宋兴国 <1328989942@qq.com>
2026-01-27 23:32:23 +0800

    修改博客地址

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -6,7 +6,7 @@ export const THEME_CONFIG: App.Locals["config"] = {
   /** website description */
   desc: "爱技术，也爱生活！",
   /** your deployed domain */
-  website: "https://songxingguo.com/blog",
+  website: "https://songxingguo.com",
   /** your locale */
   locale: "zh-cn",
   /** theme style */
